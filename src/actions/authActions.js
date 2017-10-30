992db6b4f6b943bf9025408679a23977698f3b55
caiman90 <rejhanh@outlook.com>
2017-10-30 10:41:56 +0100

    Adding flux , axios , and adding actions and dispatcher

---
+++ src/actions/authActions.js
@@ -0,0 +1,22 @@
+/**
+ * Created by rejhan on 30.10.2017.
+ */
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+
+var AuthActions = {
+    createUser: function (user) {
+        // later will implement 
+    },
+    login: function(user){
+        
+    },
+    logout: function (user) {
+        
+    },
+    loggedIn: function (user) {
+        
+    }
+};
+module.exports= AuthActions;
---
+++ src/dispatcher/appDispatcher.js
@@ -0,0 +1,7 @@
+/**
+ * Created by rejhan on 30.10.2017.
+ */
+
+var Dispatcher = require('flux').Dispatcher;
+
+module.exports = new Dispatcher();
